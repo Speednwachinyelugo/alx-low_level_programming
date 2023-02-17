bd15df27e1ab085c5d1a8c36c1d88e93ab65b11f
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-02-17 00:24:07 -0800

    6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
+
+
 /**
- * main - prints "Programming is like building a multilingual puzzle
- *
- * Return: Always 0 (Success)
- * */
+ * main - Entry point
+ * 
+ * Return: Always 0
+*/
+
 int main(void)
+
+
 {
-	puts("\" "Programming is like building a multilingual puzzle);
+	int a = 1;
+	char c = "w";
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+       	printf("Size of an int: %d byte(s)\n", sizeof(int));	
+	printf("Size of a long int: %d byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 
 }
