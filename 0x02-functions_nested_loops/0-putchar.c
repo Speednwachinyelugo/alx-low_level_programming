285e1010a7191165406ce862bb8aa2ef648357a0
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-02-21 13:54:49 -0800

    _putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-#include <main.h>
+#include <unistd.h>
+#include "main.h"
 
 /**
- * main - prints _putchar
- * followed by a new line.
- * Return: Always 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
-
 {
-	write("\"_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-
 }
