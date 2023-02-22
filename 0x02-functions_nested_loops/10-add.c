cfda90fe5279f81f35f35b33966fdb7b49aef094
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-02-22 14:07:57 -0800

    Nested loop

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * @a: input
+ * @b: input
+ * Description: function add()
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
+
