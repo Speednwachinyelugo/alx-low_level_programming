2d442f6c81caaa2702797a0ecf1f3f3565f47e4f
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-02-22 13:27:26 -0800

    Nested loop

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * print_sign - Check the code
- * @n: input character
- * Description: The function is for print_sign()
- * Return: Always 0
+ * print_sign - check main
+ * @n: input
+ * Description: function print_sign()
+ * Return: 0
  */
 
 int print_sign(int n)
 {
-	int num;
+	int neg;
 
 	if (n > 0)
 	{
 		_putchar('+');
-		num = 1;
+		neg = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		num = 0;
+		neg = 0;
 	}
 	else
-		(n < 0)
 	{
 		_putchar('-');
-		num = -1;
+		neg = -1;
 	}
-	return (num);
+	return (neg);
 }
