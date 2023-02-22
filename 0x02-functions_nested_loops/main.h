a3cfc819afc9d2a409f870cf787cff7207971177
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-02-22 14:03:27 -0800

    Nested loop

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,8 +16,8 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
-void time_table(void);
+void times_table(void);
 
-int add(int,int);
+int add(int, int);
 
-void print_times_table(int n);
+void print_to_98(int n);
