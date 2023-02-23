d450f0b7c41cbe28e90d6803e7884628d6d6ee67
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-02-23 11:25:26 -0800

    More functions_Nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - check main
+ * @c: input
+ * Desccription: function that checks for uppercase character
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	char ch;
+	int res = 0;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		if (c == ch)
+		{
+			res = 1;
+			break;
+		}
+	}
+	return (res);
+}
+
