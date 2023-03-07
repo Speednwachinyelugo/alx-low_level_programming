7aaf302d0f8bf638c38b906665157647f5d1bf7a
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-03-07 11:14:08 -0800

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,9 +5,10 @@
  * @s: A pointer to the memory area to be filled
  * @b: The character
  * @n: The number of bytes to be filled
- *
+ * Description: functiomn _isalpha()
  * Return: a pointer to the memory area @s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int j;
