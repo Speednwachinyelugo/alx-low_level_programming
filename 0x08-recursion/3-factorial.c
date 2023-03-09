c35bbac888ec83a9e8b4e22a5b352f23f430ba88
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-03-08 19:33:03 -0800

    C - Recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial: returns factorial of a number
+ * @n: the number whose factorial is needed
+ * Return: -1 if n < 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n >= 0 && n <= 1)
+		return (1);
+	return (n * factorial(n-1));
+}
+
+
