6b9a4198896408f40ce5fa8ff71e77c47fafc398
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-03-10 13:51:29 -0800

    C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,12 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
