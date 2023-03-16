41ad6c233b529612f386b4043619af7731dbb293
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-03-15 20:59:28 -0700

    C - malloc, free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,39 +2,44 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: The string to be concatenated upon.
- * @s2: The string to be concatenated to s1.
- *
- * Return: If concatenation fails - NULL.
- *         Otherwise - a pointer the newly-allocated space in memory
- *                     containing the concatenated strings.
- */
-char *str_concat(char *s1, char *s2)
+* alloc_grid - entry point
+* @width: input
+* @height: input
+* Description: function
+* Return: 0
+*/
+
+int **alloc_grid(int width, int height)
 {
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	int **arr;
+	int i, j;
 
-	if (s1 == NULL)
-		s1 = "";
+	if (height <= 0 || width <= 0)
+		return (NULL);
+	arr = (int **) malloc(sizeof(int *) * height);
 
-	if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
+	if (arr == NULL)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			for (j = 0; j <= i; j++)
+				free(arr[j]);
+			return (NULL);
+		}
+	}
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
 
-	return (concat_str);
+	return (arr);
 }
-
