0a379c99689536f8947b1cdb6b115c5797c3c0ff
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-03-21 12:36:21 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,18 +2,18 @@
 
 /**
   * init_dog - entry point
-  * @d: the dog
-  * @name: the name of the dog
-  * @age: the age of the dog
-  * @owner: the owner of the dog
+  * @d: one
+  * @name: three
+  * @age: four
+  * @owner: five
   */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-        if (d)
-        {
-        (*d).name = name;
-        (*d).age = age;
-        (*d).owner = owner;
-        }
+	if (d)
+	{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+	}
 }
