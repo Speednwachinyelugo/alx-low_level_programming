88b40780f1fbda7a2db2bede425de1c844939a8c
Izuchukwu Anakwenze <izuchukwuanakwenze@gmail.com>
2023-03-21 11:19:00 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,14 @@
 #include "dog.h"
 
+#include "dog.h"
+
 /**
- * init_dog - entry point
- * @d: The dog to be initialized.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- */
+  * init_dog - entry point
+  * @d: one
+  * @name: three
+  * @age: four
+  * @owner: five
+  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
